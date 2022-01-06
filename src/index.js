66bbff9123df51d5621c71765ddfc0aba057f415
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2022-01-06 18:58:59 +0530

    minor changes and added sockets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //Dependencies
+const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
 const server = require('http').Server(app)
@@ -10,6 +11,16 @@ app.use(express.json());
 app.use("/api/servers/", serverRoutes);
 
 //Socket Methods
+io.on("connection", socket =>{
+    socket.on("join-stream", (roomID, userID) =>{
+        socket.join(roomID);
+        socket.to(roomID).broadcast.emit('user-connected', userID);
+
+        socket.on("disconnect", () =>{
+            socket.to(roomID).broadcast.emit('user-disconnected', userID);
+        })
+    });
+});
 //Start Server
 const port = process.env.port || 1414;
 server.listen(port);
