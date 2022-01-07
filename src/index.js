95f8a3e1028e672414cf2c885ce22c1b4df3524d
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2022-01-07 15:00:25 +0530

    Jan 7 : fixed io, brushed up routes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 //Dependencies
-const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
-const server = require('http').Server(app)
-const io = require('socket.io')(server);
+const cors = require("cors");
+const server = require("http").Server(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 //Routes
 const serverRoutes = require("./routes/servers");
 app.use(express.json());
+app.use(cors());
 app.use("/api/servers/", serverRoutes);
 
 //Socket Methods
-io.on("connection", socket =>{
-    socket.on("join-stream", (roomID, userID) =>{
-        socket.join(roomID);
-        socket.to(roomID).broadcast.emit('user-connected', userID);
-
-        socket.on("disconnect", () =>{
-            socket.to(roomID).broadcast.emit('user-disconnected', userID);
-        })
-    });
+io.on("connection", (socket) => {
+  console.log("user connected");
 });
+
 //Start Server
 const port = process.env.port || 1414;
-server.listen(port);
+server.listen(port, () => {
+  console.log("listening....");
+});
