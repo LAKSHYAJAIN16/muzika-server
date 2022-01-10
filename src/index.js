5e22387db1603e1c30d502df4a957007ef6e9e90
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2022-01-10 15:45:00 +0530

    Jan 10 : Fixes up server side sockets....Still figuring out how the MediaStream API works

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const server = require("http").Server(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "CONNECT"],
+  },
+});
 
 //Routes
 const serverRoutes = require("./routes/servers");
@@ -12,13 +16,33 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/servers/", serverRoutes);
 
+//Start Server
+const port = 1414;
+server.listen(port, () => {
+  console.log("listening....");
+});
+
 //Socket Methods
 io.on("connection", (socket) => {
   console.log("user connected");
-});
 
-//Start Server
-const port = process.env.port || 1414;
-server.listen(port, () => {
-  console.log("listening....");
+  //Read for the join-room callback
+  socket.on("join-room", (roomID, userID) => {
+    //Create Payload
+    const payload= JSON.stringify({
+      roomID : roomID,
+      userID : userID
+    });
+    io.emit("user-connected", payload);
+  });
+
+  socket.on("broadcast-media", (media) =>{
+    console.log(media);
+    io.emit("broadcast", media);
+  });
+
+  //Read for the disconnect callback
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
